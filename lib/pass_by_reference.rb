8a358d506890674782f8d55343c643f9e047e4f1
deniznida <denizunat@hotmail.com>
2015-02-09 09:58:35 -0500

    done

---
+++ lib/pass_by_reference.rb
@@ -0,0 +1,10 @@
+def add_instructor(instructors, name)
+  instructors << name
+end
+
+def be_friends_with(play_pals, name)
+  new_play_pals = []
+  new_play_pals << play_pals
+  new_play_pals << name
+  new_play_pals.flatten
+end
